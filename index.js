bc9b10145b6065f05e35b4399f53c97c72d155eb
RAGHAV MUKATI <mukati_951955@student.nitw.ac.in>
2022-07-26 20:02:54 +0530

    add home message

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(cors(corsOption));
 const server = http.createServer(app);
 
 app.get('/whois', extractDomain, getWhois);
+app.get('/', (req, res) => res.json('Hello World!'));
 
 // app.use('/', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'index.html')));
 
